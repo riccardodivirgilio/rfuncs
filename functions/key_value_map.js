f29ce70db84265da602dcab8739635f7f61367bf
riccardodivirgilio <riccardod@wolfram.com>
2018-03-23 20:52:14 +0100

    renaming to key_value_map

--- functions/key_value_map.js
+++ functions/key_value_map.js
@@ -1,7 +1,7 @@
 
 import scan from './scan'
 
-export default function key_value_map(keyf, valuef, iterable) {
+export default function object_map(keyf, valuef, iterable) {
     let newobj = {}
     scan(
         (value, key) => newobj[keyf(value, key)] = valuef(value, key),
--- test/key_value_map.tests.js
+++ test/key_value_map.tests.js
@@ -1,13 +1,13 @@
 
-import key_value_map from '../functions/key_value_map'
+import object_map from '../functions/object_map'
 import compare    from './utils/compare'
 
-it("key_value_map", () => {
-  compare.as_json(key_value_map((v) => v, (v) => v+2, [1, 2, 3]), {1:3, 2:4, 3:5})
-  compare.as_json(key_value_map((v) => v, (v) => v+2, 1), {1:3})
+it("object_map", () => {
+  compare.as_json(object_map((v) => v, (v) => v+2, [1, 2, 3]), {1:3, 2:4, 3:5})
+  compare.as_json(object_map((v) => v, (v) => v+2, 1), {1:3})
 })
 
-it("key_value_map indexed", () => {
-  compare.as_json(key_value_map((v, i) => i+1, (v, i) => v+2, [4, 5, 6]), {1:6, 2:7, 3:8})
-  compare.as_json(key_value_map((v, i) => i+1, (v, i) => v+2, 3), {1:5})
+it("object_map indexed", () => {
+  compare.as_json(object_map((v, i) => i+1, (v, i) => v+2, [4, 5, 6]), {1:6, 2:7, 3:8})
+  compare.as_json(object_map((v, i) => i+1, (v, i) => v+2, 3), {1:5})
 })
