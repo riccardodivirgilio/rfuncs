c35bff42e5fca9fd622577252aafba11ddaaf8be
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-18 10:26:59 +0200

    checking mutability

--- test/sort.tests.js
+++ test/sort.tests.js
@@ -1,14 +1,23 @@
 
-import sort  from '../functions/sort'
+import sort    from '../functions/sort'
 import compare from './utils/compare'
 
 it("sort", () => {
-  compare.as_json(sort([3, 1, 2]), [1, 2, 3])
-  compare.as_json(sort([3, 1, 2], (a, b) => a > b), [1, 2, 3])
-  compare.as_json(sort([3, 1, 2], (a, b) => a < b), [3, 2, 1])
 
-  compare.as_json(sort({'a':3, 'b':1, 'c':2}), [1, 2, 3])
-  compare.as_json(sort({'a':3, 'b':1, 'c':2}, (a, b) => a > b), [1, 2, 3])
-  compare.as_json(sort({'a':3, 'b':1, 'c':2}, (a, b) => a < b), [3, 2, 1])
+  const arr = [3, 1, 2]
+  const obj = {'a':3, 'b':1, 'c':2}
+
+  compare.as_json(sort(arr), [1, 2, 3])
+  compare.as_json(sort(arr, (a, b) => a > b), [1, 2, 3])
+  compare.as_json(sort(arr, (a, b) => a < b), [3, 2, 1])
+
+  compare.as_json(sort(obj), [1, 2, 3])
+  compare.as_json(sort(obj, (a, b) => a > b), [1, 2, 3])
+  compare.as_json(sort(obj, (a, b) => a < b), [3, 2, 1])
+
+  // checking that we didn't modify original data
+
+  compare.as_json(arr, [3, 1, 2])
+  compare.as_json(obj, {'a':3, 'b':1, 'c':2})
 
 })
