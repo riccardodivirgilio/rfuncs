176ac5394715ee9026a09581021a7214986a42c2
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-18 07:57:23 +0200

    sorting imports

--- functions/nest.js
+++ functions/nest.js
@@ -1,7 +1,7 @@
 
-import repeat      from './repeat'
-import composition from './composition'
 import apply       from './apply'
+import composition from './composition'
+import repeat      from './repeat'
 
 export default function nest(f, expr, step = 1) {
     return apply(composition, repeat(f, step))(expr)
