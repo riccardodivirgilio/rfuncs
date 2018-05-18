567c5942dc53c189bd8483962e736581b48a02e5
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-18 11:01:06 +0200

    fix bug

--- functions/sort.js
+++ functions/sort.js
@@ -7,10 +7,10 @@ export default function sort(iterable, f = (a, b) => a > b) {
     // javascript sort wants you to return 1 or -1.
     // we really want a boolean check, which is why we first cast the test to bool and then we cast to an integer such us 1 / -1
 
-    const t = (a, b) => t(a, b) == true * 2 - 1
+    const t = (a, b) => f(a, b) == true * 2 - 1
 
     if (is_array(iterable)) {
-        return [].concat(iterable).sort(f)
+        return [].concat(iterable).sort(t)
     }
-    return to_array(iterable).sort(f)
+    return to_array(iterable).sort(t)
 }
