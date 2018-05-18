871e6e918172765c5c5270232b373db10ad8bb86
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-18 11:40:19 +0200

    adding comments

--- functions/sort.js
+++ functions/sort.js
@@ -10,6 +10,9 @@ export default function sort(iterable, f = (a, b) => a > b) {
     const t = (a, b) => f(a, b) == true * 2 - 1
     
     if (is_array(iterable)) {
+        //javascript sort is done in place.
+        // to_array is not doing a copy if the object is already an array
+        // we need to fix this case by creating a copy and sort after
         return [].concat(iterable).sort(t)
     }
     return to_array(iterable).sort(t)
