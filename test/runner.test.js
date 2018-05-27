e9a494146500c49d73915ea9fbc8bab8369e3ff0
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-05-27 14:25:53 +0200

    the runner is now a function

--- test/runner.test.js
+++ test/runner.test.js
@@ -1,16 +1,21 @@
+
 import spec     from '../all/spec'
 import _        from '../all/functions'
 import compare  from './utils/compare'
 
-_.scan(
-    (spec, name) => {
-        _.scan(
-            section => it(
-                section.name || name, 
-                () => section.tests(_[name], compare.as_json)
-            ),
-            spec.sections
-        )
-    },
-    spec
-)
+const runner = function() {
+    _.scan(
+        (spec, name) => {
+            _.scan(
+                section => it(
+                    section.name || name, 
+                    () => section.tests(_[name], compare.as_json)
+                ),
+                spec.sections
+            )
+        },
+        spec
+    )    
+}
+
+runner()
