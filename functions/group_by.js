bda4da408ed9fcf2f78b4e39bb1577ec65bf15df
riccardodivirgilio <riccardodivirgilio@gmail.com>
2018-06-13 12:20:01 +0200

    adding group_by implementation

---
+++ functions/group_by.js
@@ -0,0 +1,27 @@
+
+import is_function from './is_function'
+import scan        from './scan'
+import map         from './map'
+
+export default function group_by(iterable, f = a => a, merger = null) {
+
+    const getter = is_function(f) ? f : (v) => v[f]
+    const result = {}
+
+    scan(
+        (v, k) => {
+            const key = getter(v, k)
+            if (! result[key]) {
+                result[key] = []
+            }
+            result[key].push(v)
+        },
+        iterable
+    )
+
+    if (merger) {
+        return map(merger, result)
+    }
+
+    return result
+}
---
+++ spec/group_by.js
@@ -0,0 +1,46 @@
+
+export default {
+  sections: [{
+    name: 'group_by',
+    tests: (group_by, compare) => {
+
+      compare(
+        group_by(
+          [1, 1, 2, 3], 
+        ), 
+        {1: [1, 1], 2: [2], 3: [3]}
+      )    
+
+      compare(
+        group_by(
+          [{a: 2}, {a: 3}, {a:2, b:4}], 
+          "a"
+        ), 
+        {2: [{a:2}, {a:2, b:4}], 3: [{a: 3}]}
+      )  
+
+      compare(
+        group_by(
+          [{a: 2}, {a: 3}, {a:2, b:4}], 
+          v => v.a
+        ), 
+        {2: [{a:2}, {a:2, b:4}], 3: [{a: 3}]}
+      )    
+    
+    }
+  }, {
+    name: 'group_by with merger',
+    tests: (group_by, compare) => {
+
+      compare(
+        group_by(
+          [1, 1, 2, 2, 3],
+          v => v,
+          array => array.reduce((a, b) => a + b, 0)
+        ), 
+        {1: 2, 2: 4, 3: 3}
+      )    
+    
+    }
+  }]
+}
