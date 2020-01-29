fc7ad2c508017b143f50177bbc1c81e7691f916e
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2020-01-29 12:20:59 +0100

    needs to be parenthesised

--- functions/sort.js
+++ functions/sort.js
@@ -7,7 +7,7 @@ export default function sort(iterable, f = (a, b) => a > b) {
     // javascript sort wants you to return 1 or -1.
     // we really want a boolean check, which is why we first cast the test to bool and then we cast to an integer such us 1 / -1
 
-    const t = (a, b) => f(a, b) == true * 2 - 1
+    const t = (a, b) => (f(a, b) == true) * 2 - 1
     
     if (is_array(iterable)) {
         //javascript sort is done in place.
