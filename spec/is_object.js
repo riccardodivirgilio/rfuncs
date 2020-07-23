59770dfbc59c4170594e8b34f473ed08bad0018d
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2020-07-23 23:20:20 +0200

    adding test for ordered_object

--- spec/is_object.js
+++ spec/is_object.js
@@ -1,3 +1,6 @@
+
+import object_map from "../functions/object_map"
+
 export default {
   sections: [
     {
@@ -6,6 +9,7 @@ export default {
         compare(is_object(0), false)
         compare(is_object([]), false)
         compare(is_object({}), true)
+        compare(is_object(object_map(k => k + 2, k => k, [1, 2, 3])), true)
         compare(is_object(null), false)
         compare(is_object(undefined), false)
       },
