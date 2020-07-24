716d0ac6dfe59be0a572c4a896836e24ba468d4a
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2020-07-24 19:34:26 +0200

    using array filter also for objects

--- functions/filter.js
+++ functions/filter.js
@@ -1,19 +1,13 @@
 import to_array from "./to_array"
+import items from "./items"
 import is_object from "./is_object"
-import scan from "./scan"
 import ordered_object from "../functions/ordered_object"
 
 export default function filter(f, iterable) {
   // we want to return a new object if iterable is an object
 
   if (is_object(iterable)) {
-    let newobj = []
-    scan((value, key) => {
-      if (f(value, key)) {
-        newobj.push([key, value])
-      }
-    }, iterable)
-    return ordered_object(newobj)
+    return ordered_object(items(iterable).filter((a) => f(a[1], a[0])))
   }
   return to_array(iterable).filter(f)
 }
