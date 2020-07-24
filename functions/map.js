d19ced0f1a778af658da6a96a60776d9d05a2d7c
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2020-07-24 19:45:35 +0200

    using map to implement map

--- functions/map.js
+++ functions/map.js
@@ -1,9 +1,9 @@
 import to_array from "./to_array"
-import scan from "./scan"
 import is_object from "./is_object"
 import is_iterator from "./is_iterator"
 import is_function from "./is_function"
 import is_element from "./is_element"
+import items from "./items"
 import ordered_object from "../functions/ordered_object"
 
 export default function map(f, iterable) {
@@ -15,11 +15,7 @@ export default function map(f, iterable) {
     !is_function(iterable) &&
     !is_element(iterable)
   ) {
-    let newobj = []
-    scan((value, key) => {
-      newobj.push([key, f(value, key)])
-    }, iterable)
-    return ordered_object(newobj)
+    return ordered_object(items(iterable).map(a => [a[0], f(a[1], a[0])]))
   }
   return to_array(iterable).map(f)
 }
