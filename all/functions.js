1bdc6a6e23cda6f15d877bc3f6551b71c28e8e39
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2021-09-21 08:51:30 +0200

    adding items

--- all/functions.js
+++ all/functions.js
@@ -1,85 +1,86 @@
-import all from "../functions/all"
-import any from "../functions/any"
-import apply from "../functions/apply"
-import composition from "../functions/composition"
-import contains from "../functions/contains"
-import delete_duplicates from "../functions/delete_duplicates"
-import filter from "../functions/filter"
-import first from "../functions/first"
-import group_by from "../functions/group_by"
-import is_array from "../functions/is_array"
-import is_element from "../functions/is_element"
-import is_function from "../functions/is_function"
-import is_iterator from "../functions/is_iterator"
-import is_null from "../functions/is_null"
-import is_object from "../functions/is_object"
-import is_string from "../functions/is_string"
-import items from "../functions/items"
-import key_drop from "../functions/key_drop"
-import key_take from "../functions/key_take"
-import keys from "../functions/keys"
-import last from "../functions/last"
-import length from "../functions/length"
-import map from "../functions/map"
-import merge from "../functions/merge"
-import nest from "../functions/nest"
-import object_map from "../functions/object_map"
-import ordered_object from "../functions/ordered_object"
-import part from "../functions/part"
-import partial from "../functions/partial"
-import random_choice from "../functions/random_choice"
-import random_integer from "../functions/random_integer"
-import random_real from "../functions/random_real"
-import range from "../functions/range"
-import repeat from "../functions/repeat"
-import riffle from "../functions/riffle"
-import scan from "../functions/scan"
-import sort from "../functions/sort"
-import sort_by from "../functions/sort_by"
-import to_array from "../functions/to_array"
-import values from "../functions/values"
-import zip from "../functions/zip"
+
+import all from '../functions/all'    
+import any from '../functions/any'    
+import apply from '../functions/apply'    
+import composition from '../functions/composition'    
+import contains from '../functions/contains'    
+import delete_duplicates from '../functions/delete_duplicates'    
+import filter from '../functions/filter'    
+import first from '../functions/first'    
+import group_by from '../functions/group_by'    
+import is_array from '../functions/is_array'    
+import is_element from '../functions/is_element'    
+import is_function from '../functions/is_function'    
+import is_iterator from '../functions/is_iterator'    
+import is_null from '../functions/is_null'    
+import is_object from '../functions/is_object'    
+import is_string from '../functions/is_string'    
+import items from '../functions/items'    
+import key_drop from '../functions/key_drop'    
+import key_take from '../functions/key_take'    
+import keys from '../functions/keys'    
+import last from '../functions/last'    
+import length from '../functions/length'    
+import map from '../functions/map'    
+import merge from '../functions/merge'    
+import nest from '../functions/nest'    
+import object_map from '../functions/object_map'    
+import ordered_object from '../functions/ordered_object'    
+import part from '../functions/part'    
+import partial from '../functions/partial'    
+import random_choice from '../functions/random_choice'    
+import random_integer from '../functions/random_integer'    
+import random_real from '../functions/random_real'    
+import range from '../functions/range'    
+import repeat from '../functions/repeat'    
+import riffle from '../functions/riffle'    
+import scan from '../functions/scan'    
+import sort from '../functions/sort'    
+import sort_by from '../functions/sort_by'    
+import to_array from '../functions/to_array'    
+import values from '../functions/values'    
+import zip from '../functions/zip'    
 
 export default {
-  all,
-  any,
-  apply,
-  composition,
-  contains,
-  delete_duplicates,
-  filter,
-  first,
-  group_by,
-  is_array,
-  is_element,
-  is_function,
-  is_iterator,
-  is_null,
-  is_object,
-  is_string,
-  items,
-  key_drop,
-  key_take,
-  keys,
-  last,
-  length,
-  map,
-  merge,
-  nest,
-  object_map,
-  ordered_object,
-  part,
-  partial,
-  random_choice,
-  random_integer,
-  random_real,
-  range,
-  repeat,
-  riffle,
-  scan,
-  sort,
-  sort_by,
-  to_array,
-  values,
-  zip,
+    all,
+    any,
+    apply,
+    composition,
+    contains,
+    delete_duplicates,
+    filter,
+    first,
+    group_by,
+    is_array,
+    is_element,
+    is_function,
+    is_iterator,
+    is_null,
+    is_object,
+    is_string,
+    items,
+    key_drop,
+    key_take,
+    keys,
+    last,
+    length,
+    map,
+    merge,
+    nest,
+    object_map,
+    ordered_object,
+    part,
+    partial,
+    random_choice,
+    random_integer,
+    random_real,
+    range,
+    repeat,
+    riffle,
+    scan,
+    sort,
+    sort_by,
+    to_array,
+    values,
+    zip,
 }
