cca7f7a8a67f7e2b4bf0ba3bec13b6af97c8b0b5
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2023-03-09 11:34:29 +0100

    adding a getter

---
+++ functions/getter.js
@@ -0,0 +1,17 @@
+import identity from "./identity"
+import is_function from "./is_function"
+import is_null from "./is_null"
+import is_string from "./is_string"
+import map from "./map"
+
+export default function getter(e) {
+    if (is_string(e)) {
+        return o => o[e]
+    } else if (is_null(e)) {
+        return identity
+    } else if (is_function(e)) {
+        return e
+    } else {
+        return o => map(v => getter(v)(o), e)
+    }
+}
---
+++ spec/getter.js
@@ -0,0 +1,30 @@
+export default {
+    sections: [
+        {
+            name: "getter",
+            tests: (getter, compare) => {
+                compare(getter()({ a: 2 }), { a: 2 })
+
+                compare(getter("a")({ a: 2 }), 2)
+
+                compare(getter(["a", "b"])({ a: 2, b: 3 }), [2, 3])
+
+
+                compare(getter({ x: "a", z: "b" })({ a: 2, b: 3 }), {
+                    x: 2,
+                    z: 3,
+                })
+
+
+                compare(getter(a => a+2)(0), 2)
+
+                compare(getter([a => a+2, a => a+4])(0), [2, 4])
+
+                compare(getter({a: a => a+2, b: a => a+4})(0), {a: 2, b: 4})
+
+
+
+            },
+        },
+    ],
+}
